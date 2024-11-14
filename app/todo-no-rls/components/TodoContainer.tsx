bc9d8d23b2ecf8b79411ae4c37c69e2db4106ea1
Jeong Sua <sua.jeong.developer@gmail.com>
2024-11-14 16:51:30 +0900

    build: CRUD 기능 구현

--- app/todo-no-rls/components/TodoContainer.tsx
+++ app/todo-no-rls/components/TodoContainer.tsx
@@ -5,7 +5,14 @@ import TodoList from "@/components/ui/TodoList";
 import React, { useEffect } from "react";
 
 const TodoContainer = () => {
-  const { loading, todos } = useTodosController();
+  const {
+    loading,
+    todos,
+    onCreateEmptyTodos,
+    onDeleteTodos,
+    onSearchTodos,
+    onUpdateTodos,
+  } = useTodosController();
   return (
     <div>
       <TodoList
@@ -13,6 +20,18 @@ const TodoContainer = () => {
         ownerUserId="123"
         loading={loading}
         todoListData={todos}
+        isReadOnly={false}
+        // onUpdate={(id, content) => {
+        //   onUpdateTodos(id, content);
+        // }}
+        //위의 코드는 축약해서
+        onUpdate={onUpdateTodos} //이렇게 줄일 수도 있다
+        onCreate={onCreateEmptyTodos}
+        // {() => {
+        //   onCreateEmptyTodos();
+        // }}
+        onDelete={onDeleteTodos}
+        onSearch={onSearchTodos}
       />
     </div>
   );
