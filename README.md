ec196a79a26c4b47bb0c1c5bafdc5e0dca8937a3
Jeong Sua <sua.jeong.developer@gmail.com>
2024-12-23 16:55:18 +0900

    build: supabase RLS 정책 설정 및 연결

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 <!-- 대기 화면 관련 아이콘 -->
+
 yarn add react-spinners@^0.13.8
+
 <!-- 일반 아이콘들 -->
+
 yarn add react-icons@^5.0.1
+
 <!-- supabase관련 -->
+
 yarn add @supabase/supabase-js@^2.42.0
 yarn add @supabase/ssr@^0.1.0
+
 <!-- 로그인 관련 UI -->
+
 yarn add @supabase/auth-ui-react@^0.4.7  
 yarn add @supabase/auth-ui-shared@^0.1.8
+
 <!-- 언제 어디서든 next.js로 쿠키를 사용하고자 할 때 동일하게 사용이 가능 -->
+
 yarn add cookies-next@^4.1.1
-
-
--- actions/todo/todo.action.ts
+++ actions/todo/todo.action.ts
@@ -2,18 +2,81 @@
 
 import { createServerSideClient } from "@/lib/supabase";
 
-export const getTodoAction = async () => {
-  // console.log("pingAtion income");
+// todoList 가져오기
+export const getTodos = async () => {
   const supabase = await createServerSideClient();
-
   const result = await supabase
-    .from("todos_no_rls")
+    .from("todos_with_rls")
     .select("*")
     .is("deleted_at", null)
-    .order("id", {
-      ascending: false,
-    });
+    .order("id", { ascending: false });
 
-  console.log("ping GET Api income", result);
+  return result.data;
+};
+
+// todoList 가져오기 + by id
+export const getTodosById = async (id: number) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .select("*")
+    .is("deleted_at", null)
+    .eq("id", id); //특정 id 값과 동일한 데이터를 가져옴
+  return result.data;
+};
+
+// todoList 가져오기 + search
+export const getTodosBySearch = async (terms: string) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .select("*")
+    .is("deleted_at", null)
+    .ilike("content", `%${terms}`)
+    .order("id", { ascending: false })
+    .limit(500);
+  // ilike란 terms의 대소문자를 구분하지 않고 search를 진행한다는 의미
+  // 만약 해당되는 데이터가 너무 많은 경우 limit를 통해 데이터를 최대 500개까지만 받아오도록
+  return result.data;
+};
+
+// todoList 생성하기
+export const createTodos = async (content: string) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .insert({
+      content: content,
+      // 이처럼 key와 value가 같다면 그냥 content로만 써도 된다
+    })
+    .select();
+  return result.data;
+};
+
+// todoList 업데이트 하기
+export const updateTodos = async (id: number, content: string) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .update({
+      content: content,
+      updated_at: new Date().toISOString(),
+    })
+    .eq("id", id)
+    .select();
+  return result.data;
+};
+
+//todoList softDelete
+export const deleteTodosSoft = async (id: number) => {
+  const supabase = await createServerSideClient();
+  const result = await supabase
+    .from("todos_with_rls")
+    .update({
+      deleted_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+    })
+    .eq("id", id)
+    .select();
   return result.data;
 };
